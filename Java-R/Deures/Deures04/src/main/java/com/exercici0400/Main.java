a0815a74d9a3897cd427fbdaddae8624b09ba351
SayaGN09 <sabrinaelmeftah@gmail.com>
2025-03-06 16:37:22 +0100

    main

--- Java-R/Deures/Deures04/src/main/java/com/exercici0400/Main.java
+++ Java-R/Deures/Deures04/src/main/java/com/exercici0400/Main.java
@@ -3,55 +3,81 @@ package com.exercici0400;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-
+import java.util.Scanner;
 /*
- Estructura ✅
- Components ✅
- Menús
- Text
- Table
- Input
- */
-
-public class Main { 
-    public static void main(String[] args) {
-   
-
-        ArrayList<Component> components = new ArrayList<>();
-/*
-        Text txt0 = new Text(1, 1, 20, 8, "Txt0", "Lorem ipsum dolor sit amet, consectetur adipiscing elit", 3, false, "left");
-        Text txt1 = new Text(22, 1, 25, 8, "Txt1", "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", 3, false, "center");
-        components.add(txt0);
-        components.add(txt1);
+    Estructura ✅
+    Components ✅
+    Menús
+    Text
+    Table
+    Input
 */
-/* 
+
+public class Main {
+
+public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    String txt = "";
+    ArrayList<Component> components = new ArrayList<>();
+
+    while (!txt.equalsIgnoreCase("0") && !txt.equalsIgnoreCase("sortir")) {
+
         HashMap<Integer, MenuItem> mapMnu = new HashMap<>();
 
         mapMnu.put(0, new MenuItem("Sortir", new String[] {"sortir", "exit"}));
-        mapMnu.put(1, new MenuItem("Alinear esquerra", new String[]{"esquerra", "left"}));
-        mapMnu.put(2, new MenuItem("Alinear dreta", new String[]{"dreta", "right"}));
-        mapMnu.put(3, new MenuItem("Alineció centrada", new String[]{"centrat", "center"}));
+        mapMnu.put(1, new MenuItem("Esquerra", new String[]{"esquerra", "left"}));
+        mapMnu.put(2, new MenuItem("Dreta", new String[]{"dreta", "right"}));
+        mapMnu.put(3, new MenuItem("Centrada", new String[]{"centrat", "center"}));
 
-        Menu mnu0 = new Menu(1, 1, 20, 8, "Titol menú", mapMnu, false);
-        Menu mnu1 = new Menu(22, 1, 25, 8, "Titol menú", mapMnu, true);
+        Menu mnu0 = new Menu(0, 0, 20, 8, "Menú", mapMnu, true);
         components.add(mnu0);
-        components.add(mnu1);
-*/
-
-        ArrayList<String> tabHeaders0 = new ArrayList<>(Arrays.asList("col0", "col1", "col2"));
-        ArrayList<Integer> tabWidth0 = new ArrayList<>(Arrays.asList(5, 8, 10));
-        ArrayList<String> tabAligns0 = new ArrayList<>(Arrays.asList("left", "right", "center"));
-        ArrayList<ArrayList<String>> tabRows0 = new ArrayList<>(Arrays.asList(
-            new ArrayList<>(Arrays.asList("ab", "si", "gos")),
-            new ArrayList<>(Arrays.asList("def", "no", "guineu")),
-            new ArrayList<>(Arrays.asList("hi", "perquè", "llop")),
-            new ArrayList<>(Arrays.asList("jklm", "qui", "formiga"))
-        ));
-
-        Table tab0 = new Table(0, 0, 28, 8, "Taula0", tabHeaders0, tabWidth0, tabAligns0, tabRows0);
-        components.add(tab0);
 
         Container container = new Container(50, 10, components);
         container.draw();
+
+    }
+    
+    txt = scanner.nextLine();        
+    scanner.close();
+
     }
 }
+
+//         ArrayList<Component> components = new ArrayList<>();
+// /*
+//         Text txt0 = new Text(1, 1, 20, 8, "Txt0", "Lorem ipsum dolor sit amet, consectetur adipiscing elit", 3, false, "left");
+//         Text txt1 = new Text(22, 1, 25, 8, "Txt1", "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", 3, false, "center");
+//         components.add(txt0);
+//         components.add(txt1);
+// */
+// /* 
+//         HashMap<Integer, MenuItem> mapMnu = new HashMap<>();
+
+//         mapMnu.put(0, new MenuItem("Sortir", new String[] {"sortir", "exit"}));
+//         mapMnu.put(1, new MenuItem("Alinear esquerra", new String[]{"esquerra", "left"}));
+//         mapMnu.put(2, new MenuItem("Alinear dreta", new String[]{"dreta", "right"}));
+//         mapMnu.put(3, new MenuItem("Alineció centrada", new String[]{"centrat", "center"}));
+
+//         Menu mnu0 = new Menu(1, 1, 20, 8, "Titol menú", mapMnu, false);
+//         Menu mnu1 = new Menu(22, 1, 25, 8, "Titol menú", mapMnu, true);
+//         components.add(mnu0);
+//         components.add(mnu1);
+// */
+
+//         ArrayList<String> tabHeaders0 = new ArrayList<>(Arrays.asList("col0", "col1", "col2"));
+//         ArrayList<Integer> tabWidth0 = new ArrayList<>(Arrays.asList(5, 8, 10));
+//         ArrayList<String> tabAligns0 = new ArrayList<>(Arrays.asList("left", "right", "center"));
+//         ArrayList<ArrayList<String>> tabRows0 = new ArrayList<>(Arrays.asList(
+//             new ArrayList<>(Arrays.asList("ab", "si", "gos")),
+//             new ArrayList<>(Arrays.asList("def", "no", "guineu")),
+//             new ArrayList<>(Arrays.asList("hi", "perquè", "llop")),
+//             new ArrayList<>(Arrays.asList("jklm", "qui", "formiga"))
+//         ));
+
+//         Table tab0 = new Table(0, 0, 28, 8, "Taula0", tabHeaders0, tabWidth0, tabAligns0, tabRows0);
+//         components.add(tab0);
+
+//         Container container = new Container(50, 10, components);
+//         container.draw();
+//     }
