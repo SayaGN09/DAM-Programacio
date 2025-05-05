6d9e1d5fba4d107c295ea893471d7f177caa303a
SayaGN09 <150825292+SayaGN09@users.noreply.github.com>
2025-05-05 18:00:12 +0200

    Refactor ControllerConsole and ControllerGames classes; implement character view functionality and enhance image loading.

--- Java-R/Deures/Deures06/src/main/java/com/exercici0601/ControllerGames.java
+++ Java-R/Deures/Deures06/src/main/java/com/exercici0601/ControllerGames.java
@@ -1,1 +1,28 @@
 package main.java.com.exercici0601;
+
+import com.utils.*;
+
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.Parent;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+
+public class ControllerGames implements Initializable {
+    
+}
